221f02573f36821d0dcd234bd2ac29f6b9bf5f1e
Macharete_r <rafaelmacharete00@gmail.com>
2025-02-12 23:20:19 -0300

    styling the Header

--- react/react-project/src/App.jsx
+++ react/react-project/src/App.jsx
@@ -4,10 +4,10 @@ import Header from './components/Header';
 export default function App() {
   return (
     <>
+      <Header />
       <h1>My main Goal is:</h1>
       <h2>To be a Full Stack Developer</h2>
-      <h1 className="victor">Victor</h1>
-      <Header />
+      <h2>In addition, I want to be known for who I am and what I've done.</h2>
     </>
   );
 }
